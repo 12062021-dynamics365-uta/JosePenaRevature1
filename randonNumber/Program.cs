e8d394292cff67b77461279ef688a10284baa299
josepena67 <jospenmuss@hotmail.com>
2021-12-08 09:19:17 -0500

    Quick update
    Codo testing is in progress.

---
+++ randonNumber/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace randonNumber
+{
+    class Program
+    {
+        private static int sccisors;
+        private static int paper;
+
+        static void Main(string[] args)
+        {
+
+
+            int Rock = 1;
+            int Paper = 2;
+            int scissors = 3;
+            Random ranNum = new Random();
+            int hidenNumber = ranNum.Next(3);
+            int matchNumber;
+
+
+            Console.WriteLine("Please choose: Number 1 for Rock, Number 2 for Paper and Number 3 for Scissors:");
+            matchNumber = Convert.ToInt32(Console.ReadLine());
+            if (matchNumber == Rock && hidenNumber == Rock)
+            {
+                Console.WriteLine("Rock and Rock is a tie");
+            }
+            else if (matchNumber == paper && hidenNumber == sccisors)
+            {
+
+
+                Console.WriteLine("Scissors wins vs paper");
+            }
+            else if ( matchNumber == Rock && hidenNumber == scissors)
+            {
+
+                Console.WriteLine("Paper wins againt rock");
+            }
+
+            else { 
+
+            Console.WriteLine("You are the winner" + hidenNumber);
+        }
+
+
+
+        }
+    }
+}
